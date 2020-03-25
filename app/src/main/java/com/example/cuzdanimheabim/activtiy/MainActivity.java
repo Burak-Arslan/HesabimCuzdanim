7719d012ea457172012145f0bf0ead6ca491835c
Burak Arslan <43721185+Burak-Arslan@users.noreply.github.com>
2020-03-25 21:19:39 +0300

    Initial commit

---
+++ app/src/main/java/com/example/cuzdanimheabim/activtiy/MainActivity.java
@@ -0,0 +1,72 @@
+package com.example.cuzdanimheabim.activtiy;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.WindowManager;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.cuzdanimheabim.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    private static int SPLASH_SCREEN = 5000;
+
+    Animation topAnimation, bottomAnimation;
+    ImageView imgSplash;
+    TextView txtOnSoz, txtCuzdanimHesabim;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
+        setContentView(R.layout.activity_main);
+
+        init();
+        initAnimation();
+        initHandlerIntent();
+    }
+
+    private void init() {
+        try {
+            topAnimation = AnimationUtils.loadAnimation(this, R.anim.top_animation);
+            bottomAnimation = AnimationUtils.loadAnimation(this, R.anim.bottom_animation);
+
+            txtCuzdanimHesabim = findViewById(R.id.txtCuzdanimHesabim);
+            txtOnSoz = findViewById(R.id.txtOnSoz);
+            imgSplash = findViewById(R.id.imgSplash);
+        } catch (Exception ex) {
+            Toast.makeText(this, ex.getMessage(), Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    private void initAnimation() {
+        try {
+            imgSplash.setAnimation(topAnimation);
+            txtOnSoz.setAnimation(bottomAnimation);
+            txtCuzdanimHesabim.setAnimation(bottomAnimation);
+        } catch (Exception ex) {
+            Toast.makeText(this, ex.getMessage(), Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    private void initHandlerIntent() {
+        try {
+            new Handler().postDelayed(new Runnable() {
+                @Override
+                public void run() {
+                    Intent loginIntent = new Intent(MainActivity.this, LoginActivity.class);
+                    startActivity(loginIntent);
+                }
+            }, SPLASH_SCREEN);
+        } catch (Exception ex) {
+            Toast.makeText(this, ex.getMessage(), Toast.LENGTH_SHORT).show();
+        }
+    }
+}
