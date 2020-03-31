62d5aa737411d69601fe36bb0a13424e23757658
Burak Arslan <43721185+Burak-Arslan@users.noreply.github.com>
2020-04-01 00:04:50 +0300

    - drawer layout ve navigationView tasarımı tamamlandı

--- app/src/main/java/com/example/cuzdanimheabim/activtiy/MainActivity.java
+++ app/src/main/java/com/example/cuzdanimheabim/activtiy/MainActivity.java
@@ -1,37 +1,135 @@
 package com.example.cuzdanimheabim.activtiy;
 
 import android.os.Bundle;
+import android.view.MenuItem;
+import android.view.View;
 import android.view.WindowManager;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.view.GravityCompat;
 import androidx.drawerlayout.widget.DrawerLayout;
 
 import com.example.cuzdanimheabim.R;
 import com.google.android.material.navigation.NavigationView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
 
+    static final float END_SCALE = 0.7f;
 
     DrawerLayout drawerLayout;
     NavigationView navigationView;
+    ImageView imgMenu;
+    LinearLayout contentView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_main);
+
         init();
+        events();
+        navigationDrawerInit();
+        animateNavigationDrawer();
     }
+
 
     private void init() {
         try {
-
             drawerLayout = findViewById(R.id.drawerMainLayout);
             navigationView = findViewById(R.id.navigationMainView);
+            imgMenu = findViewById(R.id.imgMenu);
+            contentView = findViewById(R.id.lnrContent);
+        } catch (Exception ex) {
+            Toast.makeText(this, ex.getMessage(), Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    private void events() {
+        try {
+            imgMenu.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    try {
+                        if (drawerLayout.isDrawerVisible(GravityCompat.START)) {
+                            drawerLayout.closeDrawer(GravityCompat.START);
+                        } else {
+                            drawerLayout.openDrawer(GravityCompat.START);
+                        }
+                    } catch (Exception ex) {
+                        //
+                    }
+                }
+            });
+        } catch (Exception ex) {
+            Toast.makeText(this, ex.getMessage(), Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    private void navigationDrawerInit() {
+        try {
+            navigationView.bringToFront();
+            navigationView.setNavigationItemSelectedListener(this);
+            navigationView.setCheckedItem(R.id.test1);
 
         } catch (Exception ex) {
             Toast.makeText(this, ex.getMessage(), Toast.LENGTH_SHORT).show();
         }
+    }
+
+    private void animateNavigationDrawer() {
+        try {
+            drawerLayout.setScrimColor(getResources().getColor(R.color.colorPrimaryDark));
+            drawerLayout.addDrawerListener(new DrawerLayout.DrawerListener() {
+                @Override
+                public void onDrawerSlide(@NonNull View drawerView, float slideOffset) {
+
+                    final float diffScaleOffset = slideOffset * (1 - END_SCALE);
+                    final float offsetScale = 1 - diffScaleOffset;
+                    contentView.setScaleX(offsetScale);
+                    contentView.setScaleY(offsetScale);
+
+                    final float xOffset = drawerView.getWidth() * slideOffset;
+                    final float xOffsetDiff = contentView.getWidth() * diffScaleOffset / 2;
+                    final float xTranslation = xOffset - xOffsetDiff;
+                    contentView.setTranslationX(xTranslation);
+                }
+
+                @Override
+                public void onDrawerOpened(@NonNull View drawerView) {
+
+                }
+
+                @Override
+                public void onDrawerClosed(@NonNull View drawerView) {
+
+                }
+
+                @Override
+                public void onDrawerStateChanged(int newState) {
+
+                }
+            });
+        } catch (Exception ex) {
+            Toast.makeText(this, ex.getMessage(), Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    @Override
+    public void onBackPressed() {
+        if (drawerLayout.isDrawerVisible(GravityCompat.START)) {
+            drawerLayout.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+        }
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+        return true;
     }
 }
